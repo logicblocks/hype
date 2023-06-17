52df95f1d17e7d41255f53039593376a13174fd9
Circle CI <circleci@logicblocks.io>
2023-06-17 10:49:09 +0000

    Pre-release version 1.1.0-RC8 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/hype "1.1.0-RC7"
+(defproject io.logicblocks/hype "1.1.0-RC8"
   :description "Hypermedia functions for bidi and ring."
   :url "https://github.com/logicblocks/hype"
 
