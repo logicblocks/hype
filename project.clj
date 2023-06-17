c881c0924222148d19ed3c98413e04968a5f1d41
Circle CI <circleci@logicblocks.io>
2023-06-17 13:16:49 +0000

    Pre-release version 2.0.1-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/hype "2.0.0"
+(defproject io.logicblocks/hype "2.0.1-RC1"
   :description "Hypermedia functions for ring routers."
   :url "https://github.com/logicblocks/hype"
 
